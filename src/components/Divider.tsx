75bfde16ba05ca2213ce8f3824c7035b384d72fa
deadpine <hey@deadpine.xyz>
2025-06-26 20:34:44 -0300

    Update Divider.tsx

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -7,12 +7,12 @@ interface DividerProps {
 
 export default function Divider({ quote, bgColor = "" }: DividerProps) {
   return (
-    <section className={`w-full py-24 ${bgColor}`}>
+    <div className={`w-full py-24 ${bgColor}`}>
       <div className='w-full max-w-7xl mx-auto grid grid-cols-3 gap-6'>
         <Image src="/images/ic-star.svg" alt="tiny star" width={14} height={14} 
           className="max-h-3 my-4" />
         <h3 className="col-span-2">{quote}</h3>
       </div>
-    </section>
+    </div>
   );
 } 
